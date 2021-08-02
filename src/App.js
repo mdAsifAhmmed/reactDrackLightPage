c3a5cecb753e01cf155d31720ad4da291bb47bac
mdAsifAhmmed <mdasifahmed313@gmail.com>
2021-08-03 05:08:21 +0600

    react drage and light mode web page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Splash from "./components/SplashScreen";
+import { ThemeProvider } from "styled-components";
+
+const LightTheme = {
+  pageBackground: "White",
+  titleColor: "#dc658b",
+  tagLineColor: "black",
+};
+
+
+const DrakTheme = {
+  pageBackground: "#282c36",
+  titleColor: "#lightpink",
+  tagLineColor: "lavender",
+};
+const themes = {
+  light: LightTheme,
+  dark: DrakTheme,
+};
 
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <Splash theme={theme} setTheme={setTheme} />
+    </ThemeProvider>
   );
 }
 
